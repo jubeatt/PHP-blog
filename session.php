7d26ca8200598b43904c59275eff151531a53136
jim <jimdevelopesite@gmail.com>
2022-04-29 16:06:00 +0800

    debug

--- session.php
+++ session.php
@@ -1,11 +1,12 @@
 <?php
-  session_set_cookie_params([
-    'lifetime' => 3600,
-    'path' => '/peanu/blog/',
-    'domain' => 'localhost',
-    'secure' => TRUE,
-    'httponly' => TRUE,
-    'samesite' => 'Lax'
-  ]);
+  // 防止 CSRF 的設定
+  // session_set_cookie_params([
+  //   'lifetime' => 3600,
+  //   'path' => '/peanu/blog/',
+  //   'domain' => 'localhost',
+  //   'secure' => TRUE,
+  //   'httponly' => TRUE,
+  //   'samesite' => 'Lax'
+  // ]);
   session_start();
 ?>
